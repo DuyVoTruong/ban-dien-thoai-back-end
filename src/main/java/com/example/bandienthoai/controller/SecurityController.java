7949dbb3be74405eb02ac9ef4f0ecbdd36d62421
VoDuy89123 <votruongduy1943@gmail.com>
2024-04-10 11:55:43 +0700

    update project

--- src/main/java/com/example/bandienthoai/controller/SecurityController.java
+++ src/main/java/com/example/bandienthoai/controller/SecurityController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://localhost:3000", "https://ban-dien-thoai-front-end.vercel.app/"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://ban-dien-thoai-front-end.vercel.app"})
 public class SecurityController {
     @Autowired
     private AuthenticationManager authenticationManager;
